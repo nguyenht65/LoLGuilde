c8ae083ddfe20b3b7a47187be5b23f5c24f9f637
Trung Nguyen <nguyenht65.se@gmail.com>
2021-12-03 00:38:29 +0700

    clean code

--- LoLib/Controllers/LOL/Items/ItemsViewModel.swift
+++ LoLib/Controllers/LOL/Items/ItemsViewModel.swift
@@ -54,15 +54,10 @@ class ItemsViewModel: ItemsViewModelProtocol {
             .distinctUntilChanged() // If they didn't occur, check if the new value is the same as
             .flatMapLatest { [weak self] query -> Observable<[Item]> in
                 guard let self = self else { return .just([]) }
-                if query.isEmpty { // case when use not search anythings
+                if query.isEmpty {
                     return self.items.asObservable()
                 }
                 return self.searchItems(query)
-//                else if searchBar.selectedScopeButtonIndex == 0 {
-//                    return self.searchItems(query)
-//                } else {
-//                    return self.searchByColor(query)
-//                }
             }
             .bind(to: searchResults)
             .disposed(by: disposeBag)
@@ -93,30 +88,5 @@ class ItemsViewModel: ItemsViewModelProtocol {
         }
         return Observable.of(listItems)
     }
-    
-//    func searchByName(_ query: String) -> Observable<[Item]> {
-//        let listItems: [Item] = items.value
-//            .filter { $0.name.uppercased().contains(query.uppercased()) }
-//        return Observable.of(listItems)
-//    }
-//
-//    func searchByColor(_ query: String) -> Observable<[Item]> {
-//        var listItems: [Item] = []
-//        for item in items.value {
-//            if item.into?.isEmpty != nil && item.from?.isEmpty != nil && "yellow".contains(query.lowercased()) {
-//                // middle items (items that can be both combined into other items and be combined by other items)
-//                listItems.append(item)
-//            } else if item.into?.isEmpty != nil && item.from?.isEmpty == nil && "gray".contains(query.lowercased()) {
-//                // component items (items can be combined into other items)
-//                listItems.append(item)
-//            } else if item.into?.isEmpty == nil && item.from?.isEmpty != nil && "pink".contains(query.lowercased()) {
-//                // completed items (item that only be conbined from other items)
-//                listItems.append(item)
-//            } else if item.into?.isEmpty == nil && item.from?.isEmpty == nil && "mint".contains(query.lowercased()) {
-//                // unique items
-//                listItems.append(item)
-//            }
-//        }
-//        return Observable.of(listItems)
-//    }
+
 }
